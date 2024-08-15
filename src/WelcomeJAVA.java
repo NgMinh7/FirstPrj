54cb0410b2d00ed30754625eca01b4182cbd9c36
Minhtayto <nguyenphamcongminh07@gmail.com>
2024-08-16 01:56:46 +0700

    First Commit.

---
+++ src/WelcomeJAVA.java
@@ -0,0 +1,6 @@
+public class WelcomeJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Pham Cong Minh!");
+        System.out.println("Hello DHKTPM19B - 420300359104");
+    }
+}
